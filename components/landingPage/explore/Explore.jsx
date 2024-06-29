73787bf492f9d4b95a871fa7812609f3d8169c86
kapil <kapil.personal9711@gmail.com>
2024-06-29 10:19:44 +0530

    created about section

--- components/landingPage/explore/Explore.jsx
+++ components/landingPage/explore/Explore.jsx
@@ -1,10 +1,12 @@
+import About from "./About";
+
 const Explore = () => {
   return (
     <main
       id="explore"
       className="py-16 min-h-screen border-4 border-orange-500"
     >
-      <section className="explore">this is a main section</section>
+      <About />
     </main>
   );
 };
