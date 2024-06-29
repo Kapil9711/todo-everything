464bfd18bdb08a1cf085c8b80fa0fd040b20ea3d
kapil <kapil.personal9711@gmail.com>
2024-06-29 18:23:10 +0530

    corrected styles

--- components/landingPage/header/Content.jsx
+++ components/landingPage/header/Content.jsx
@@ -32,12 +32,10 @@ const Content = () => {
     });
   });
   return (
-    <section className="z-30  content border-2 border-orange-500 px-2 py-4 absolute right-0 bottom-1 sm:bottom-12 sm:w-5/12 ">
+    <section className="z-30  content border-2 border-orange-500 px-2 py-4 absolute right-0 sm:right-24 bottom-1 sm:bottom-12 sm:w-96 ">
       <p className="sm:leading-7 text-sm  sm:text-lg">
         Welcome to Listify, your ultimate companion for staying organized and
-        productive! We understand that managing tasks, projects, and daily
-        activities can be overwhelming, which is why we created this platform to
-        help you streamline your life and achieve your goals with ease.
+        productive!
       </p>
       <button>
         <Link
