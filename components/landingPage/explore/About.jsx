3eb0530d37958aae1ec58eed577875576a215c02
kapil <kapil.personal9711@gmail.com>
2024-07-01 16:08:58 +0530

    about

--- components/landingPage/explore/About.jsx
+++ components/landingPage/explore/About.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import Image from "next/image";
@@ -53,39 +54,45 @@ const About = () => {
   return (
     <section
       id="about"
-      className="relative sm:container pb-40 gap-16 flex items-center flex-col  mx-auto min-h-screen about"
+      className="relative sm:container pb-40 gap-16 sm:flex sm:items-center sm:flex-col  mx-auto min-h-screen about"
     >
-      <h2 className="text-3xl sm:text-5xl text-center  text-orange-400">
+      <h2 className="  text-3xl sm:text-5xl text-center  text-orange-400">
         About Us
       </h2>
       <div
         style={{ height: "500px" }}
-        className=" w-2 relative bg-orange-500 border border-orange-500 "
+        className=" w-2 sm:relative sm:bg-orange-500 sm:border sm:border-orange-500 "
       >
         <div
           // id="a"
-          className="absolute top-2 left-0 w-32 h-32 border-t-4 rounded-full border-orange-500"
+          className="hidden sm:block absolute top-2 left-0 w-32 h-32 border-t-4 rounded-full border-orange-500"
         ></div>
         <div
           // id="b"
-          className="w-32 absolute top-48 -left-32 h-32  border-t-4 rounded-full border-orange-500"
+          className="w-32 hidden sm:block absolute top-48 -left-32 h-32  border-t-4 rounded-full border-orange-500"
         ></div>
         <div
           // id="c"
-          className="w-32 h-32 absolute -bottom-16  left-0 border-t-4 rounded-full   border-orange-500"
+          className="w-32 hidden sm:block h-32 absolute -bottom-16  left-0 border-t-4 rounded-full   border-orange-500"
         ></div>
 
-        <div id="a" className="absolute top-16 left-40 w-96 ">
+        <div
+          id="a"
+          className="mt-10 sm:mt-0 sm:absolute sm:top-16 sm:left-40 w-96"
+        >
           <strong className="text-xl sm:text-2xl">Introduction :</strong>
-          <p className="max-w-96 mx-auto  p-2  mt-4  border border-orange-500 leading-7 tracking-wide text-gray-400 hover:text-gray-300 sm:text-xl sm:pl-4 sm:max-w-2/3  sm:hover:scale-105 transition-all duration-500 ease-in-out">
+          <p className=" mx-auto  p-2  mt-4  border border-orange-500 leading-7 tracking-wide text-gray-400 hover:text-gray-300 sm:text-xl sm:pl-4 sm:max-w-2/3  sm:hover:scale-105 transition-all duration-500 ease-in-out">
             Welcome to Listify, your go-to platform for staying organized and
             productive. Whether you're managing personal tasks, professional
             projects, or collaborative efforts, we've got you covered
           </p>
         </div>
-        <div id="b" className="absolute top-56 -left-96 w-96 -ml-40">
+        <div
+          id="b"
+          className=" mt-8 sm:mt:0 sm:absolute top-56 -left-96 w-96 sm:-ml-40"
+        >
           <strong className="text-xl sm:text-2xl">Our Mission :</strong>
-          <p className="max-w-96 mx-auto  sm:pl-4  p-2 mt-4  border border-orange-500 leading-7 tracking-wide text-gray-400 hover:text-gray-300 sm:text-xl sm:max-w-2/3  sm:hover:scale-105 transition-all duration-500 ease-in-out">
+          <p className="  mx-auto  sm:pl-4  p-2 mt-4  border border-orange-500 leading-7 tracking-wide text-gray-400 hover:text-gray-300 sm:text-xl sm:max-w-2/3  sm:hover:scale-105 transition-all duration-500 ease-in-out">
             Our mission is to make task management effortless for everyone. We
             aim to empower individuals and teams to achieve their goals and
             manage their time more effectively with a seamless, user-friendly
@@ -93,9 +100,9 @@ const About = () => {
           </p>
         </div>
 
-        <div id="c" className="absolute top-96 left-40 w-96 mt-28">
+        <div id="c" className="mt-8 sm:absolute top-96 left-40 w-96 sm:mt-28">
           <strong className="text-xl sm:text-2xl">Our Story :</strong>
-          <p className="max-w-96 mx-auto  sm:pl-4  p-2 mt-4  border border-orange-500 leading-7 tracking-wide text-gray-400 hover:text-gray-300 sm:text-xl sm:max-w-2/3  sm:hover:scale-105 transition-all duration-500 ease-in-out">
+          <p className=" mx-auto  sm:pl-4  p-2 mt-4  border border-orange-500 leading-7 tracking-wide text-gray-400 hover:text-gray-300 sm:text-xl sm:max-w-2/3  sm:hover:scale-105 transition-all duration-500 ease-in-out">
             Founded in 2024, Listify was born out of a simple idea: to create an
             intuitive and powerful to-do list app that anyone can use.
           </p>
