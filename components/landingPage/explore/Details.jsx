7bb20d3a2f354188163f5f78bafe9ba3761613d4
kapil <kapil.personal9711@gmail.com>
2024-07-01 11:06:55 +0530

    inital animation gsap done

--- components/landingPage/explore/Details.jsx
+++ components/landingPage/explore/Details.jsx
@@ -1,4 +1,90 @@
+"use client";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+
 const Details = () => {
+  useGSAP(() => {
+    gsap.registerPlugin({ ScrollTrigger });
+    gsap.from(".a", {
+      scrollTrigger: {
+        trigger: ".a",
+        start: "-10% bottom",
+        toggleActions: "restart none none none",
+      },
+
+      y: 100,
+      opacity: 0,
+      duration: 1,
+    });
+
+    gsap.from(".b", {
+      scrollTrigger: {
+        trigger: ".b",
+        start: "-10% bottom",
+        toggleActions: "restart none none none",
+      },
+
+      x: -100,
+      opacity: 0,
+      duration: 1,
+      delay: 0.2,
+    });
+
+    gsap.from(".c", {
+      scrollTrigger: {
+        trigger: ".c",
+        start: "-10% bottom",
+        toggleActions: "restart none none none",
+      },
+
+      x: 100,
+      opacity: 0,
+      duration: 1,
+      delay: 0.2,
+    });
+
+    gsap.from(".d", {
+      scrollTrigger: {
+        trigger: ".d",
+        start: "-10% bottom",
+        toggleActions: "restart none none none",
+      },
+
+      x: -100,
+      opacity: 0,
+      duration: 1,
+      delay: 0.4,
+    });
+
+    gsap.from(".e", {
+      scrollTrigger: {
+        trigger: ".e",
+        start: "-10% bottom",
+        toggleActions: "restart none none none",
+      },
+
+      x: 100,
+      opacity: 0,
+      duration: 1,
+      delay: 0.4,
+    });
+
+    gsap.from(".f", {
+      scrollTrigger: {
+        trigger: ".f",
+        start: "-10% bottom",
+        toggleActions: "restart none none none",
+      },
+
+      y: 50,
+      opacity: 0,
+      duration: 1,
+      delay: 0.5,
+    });
+  });
+
   return (
     <section className="relative details sm:container min-h-screen py-28 mx-auto">
       <h2
@@ -8,21 +94,21 @@ const Details = () => {
         Features
       </h2>
 
-      <div className="sm:hover:scale-105 transition-transform duration-200 ease-linear absolute top-52 left-1/2 -translate-x-1/2 w-64 h-64 flex justify-center items-center border  border-l-4 border-t-8 border-r-4 rounded-full border-orange-500">
+      <div className="a sm:hover:scale-105 transition-transform duration-200 ease-linear absolute top-52 left-1/2 -translate-x-1/2 w-64 h-64 flex justify-center items-center border  border-l-4 border-t-8 border-r-4 rounded-full border-orange-500">
         <p className="text-lg p-8 text-gray-200">
           Quickly add tasks with due dates, priorities, and notes. Effortlessly
           edit or delete tasks as needed.
         </p>
       </div>
 
-      <div className="sm:hover:scale-105 transition-transform duration-200 ease-linear  w-64 h-64 absolute  mt-40 top-96 right-80 flex justify-center items-center border   border-t-8 border-r-4 border-l-4 rounded-full border-orange-500">
+      <div className="d sm:hover:scale-105 transition-transform duration-200 ease-linear  w-64 h-64 absolute  mt-40 top-96 right-80 flex justify-center items-center border   border-t-8 border-r-4 border-l-4 rounded-full border-orange-500">
         <p className="text-lg p-8 text-gray-200">
           Break down larger tasks into manageable subtasks to ensure every
           detail is covered
         </p>
       </div>
 
-      <div className="sm:hover:scale-105 transition-transform duration-200 ease-linear  w-64 h-64 absolute mt-40 top-96 left-80 flex justify-center items-center border  border-l-4 border-t-8 border-r-4 rounded-full border-orange-500">
+      <div className="e sm:hover:scale-105 transition-transform duration-200 ease-linear  w-64 h-64 absolute mt-40 top-96 left-80 flex justify-center items-center border  border-l-4 border-t-8 border-r-4 rounded-full border-orange-500">
         <p className="text-lg p-8 text-gray-200">
           Organize your tasks with categories and tags for easy filtering and
           searching.
@@ -37,21 +123,21 @@ const Details = () => {
       </div> */}
       <div
         style={{ borderRadius: "50%" }}
-        className="sm:hover:scale-105 transition-transform duration-200 ease-linear flex justify-center items-center absolute rotate-45  ml-36 right-96 mr-10 mt-12 line1 top-96 w-60 h-28 border-t-2 border-b-2 border-orange-500"
+        className=" b sm:hover:scale-105 transition-transform duration-200 ease-linear flex justify-center items-center absolute rotate-45  ml-36 right-96 mr-10 mt-12 line1 top-96 w-60 h-28 border-t-2 border-b-2 border-orange-500"
       >
         <strong className="text-sm text-gray-200">Subtasks</strong>
       </div>
 
       <div
         style={{ borderRadius: "50%" }}
-        className="sm:hover:scale-105 transition-transform duration-200 ease-linear flex justify-center items-center absolute -rotate-45  left-96 ml-10  mt-12 line1 top-96 w-60 h-28 border-t-2 border-b-2 border-orange-500"
+        className="c sm:hover:scale-105 transition-transform duration-200 ease-linear flex justify-center items-center absolute -rotate-45  left-96 ml-10  mt-12 line1 top-96 w-60 h-28 border-t-2 border-b-2 border-orange-500"
       >
         <strong className="text-sm text-gray-200">Customization</strong>
       </div>
 
       <div
         style={{ borderRadius: "100%" }}
-        className="sm:hover:scale-105 transition-transform duration-200 ease-linear  absolute  flex justify-center items-center  left-96 ml-20  mt-12 line1 bottom-0 w-1/3 h-60 border-t-4 border-b-4 border-orange-500"
+        className="f sm:hover:scale-105 transition-transform duration-200 ease-linear  absolute  flex justify-center items-center  left-96 ml-20  mt-12 line1 bottom-0 w-1/3 h-60 border-t-4 border-b-4 border-orange-500"
       >
         <p className=" text-lg pl-20 pr-16 text-gray-200">
           Organize your tasks with categories and tags for easy filtering and
