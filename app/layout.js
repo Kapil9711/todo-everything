0621e9491172843ba6150cbb3bac3df29e29abe5
kapil <kapil.personal9711@gmail.com>
2024-07-04 18:08:10 +0530

    a

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <html style={{ scrollBehavior: "smooth" }} lang="en">
         <body>
           {/* <Navbar /> */}
