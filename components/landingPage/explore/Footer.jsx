1f8760377b1eeedeec4aa57247fe0b7cde0ffffb
kapil <kapil.personal9711@gmail.com>
2024-07-08 21:47:47 +0530

    updated footer

--- components/landingPage/explore/Footer.jsx
+++ components/landingPage/explore/Footer.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className=" py-4 min-h-44 w-full border border-white">
-      <h2 className="text-4xl sm:text-5xl text-center text-orange-500">
+    <footer className=" flex justify-center items-end py-4 min-h-32 w-full ">
+      <p className="text-center">&copy;copyright, 2024</p>
+      {/* <h2 className="text-4xl sm:text-5xl text-center text-orange-500">
         Footer
       </h2>
       <div className="mt-4 mx-auto    max-w-44">
@@ -13,7 +14,7 @@ const Footer = () => {
           <li>Features</li>
           <li>Dashboard</li>
         </ul>
-      </div>
+      </div> */}
     </footer>
   );
 };
